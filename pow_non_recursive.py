35b2ab978173f0201f926417424b9c28429620c0
bob81135 <bob81135@gmail.com>
2020-07-17 17:20:46 +0800

    pow example recursive and non_recursive

--- pow_non_recursive.py
+++ pow_non_recursive.py
@@ -1,4 +1,4 @@
-def pow(n,m):
+def pow(n,m):  #x>0 and y>=0
     ans = 1
     for _ in range(1,m+1):
         ans*=n
--- pow_recursive.py
+++ pow_recursive.py
@@ -1,4 +1,4 @@
-def pow(n,m):
+def pow(n,m): #x>0 and y>=0
     if(m==0):
         return 1
     else:
