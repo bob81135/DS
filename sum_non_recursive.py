47dded580ffb1a1fc3a6718e0691e0d648e96106
bob81135 <bob81135@gmail.com>
2020-07-17 17:16:27 +0800

    sum example recursive and non_recursive

---
+++ sum_non_recursive.py
@@ -0,0 +1,6 @@
+def sum(n):
+    ans = 0
+    for i in range(n+1):
+        ans+=i
+    return ans
+print(sum(5))
---
+++ sum_recursive.py
@@ -0,0 +1,6 @@
+def sum(n):
+    if(n==0):
+        return 0
+    else:
+        return sum(n-1)+n
+print(sum(5))
